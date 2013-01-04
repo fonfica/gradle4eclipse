6419725c3961fa694c394daa9f7a6e19cd118b58
Filip Muncan <mz@erv.de>
2013-01-04 11:30:32 +0100

    Initial commit

--- src/gradle4eclipse/Activator.java
+++ src/gradle4eclipse/Activator.java
@@ -24,7 +24,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "Gradle4Eclipse"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "gradle4eclipse"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
